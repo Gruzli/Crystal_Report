37acafaa16688fc5331346a6a0328760c230052c
Gregorius <forvalem3@gmail.com>
2024-05-31 18:29:05 +0700

    Add project files.

---
+++ Session10/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Session10")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Session10")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7fb97be1-e553-4985-9469-93bfa89089ff")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Session10/View/WebForm1.aspx.cs
@@ -0,0 +1,50 @@
+﻿using Session10.Dataset;
+using Session10.Model;
+using Session10.Report1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Session10.View
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            CrystalReport1 report1 = new CrystalReport1();
+            CrystalReportViewer1.ReportSource = report1;
+            Database1Entities db = new Database1Entities();
+            DataSet1 data = getData(db.Transactions.ToList());
+            report1.SetDataSource(data);
+        }
+
+        private DataSet1 getData(List<Transaction> transactions)
+        {
+            DataSet1 data = new DataSet1();
+            var headertable = data.Transaction;
+            var detailtable = data.TransactionDetail;
+
+            foreach (Transaction t in transactions)
+            {
+                var headerRow = headertable.NewRow();
+                headerRow["Id"] = t.Id;
+                headerRow["CustomerId"] = t.CustomerId;
+                headerRow["Date"] = t.Date;
+                headertable.Rows.Add(headerRow);
+
+                foreach (TransactionDetail d in t.TransactionDetails)
+                {
+                    var detailRow = detailtable.NewRow();
+                    detailRow["TransactionId"] = d.TransactionId;
+                    detailRow["FoodId"] = d.FoodId;
+                    detailRow["Quantity"] = d.Quantity;
+                    detailtable.Rows.Add(detailRow);
+                }
+            }
+            return data;
+        }
+    }
+}
